3816ccba406db12d32ada3f80264d7cbe3e396b0
onkar dudhale <dudhaleonkar@gmail.com>
2025-10-06 15:05:31 +0530

    change locaton of serviceWReg file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
